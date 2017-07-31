b4589020e9c0e7cf976446ce6fb681c377937b6b
jasontan0649 <jasontankaiyuan@gmail.com>
2017-07-31 21:07:22 +0800

    Add files via upload

---
+++ RX2.ino
@@ -0,0 +1,101 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+RH_ASK driver(2000,2,12,8,false);//constructor to change any default setting/pins
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8,9,10,11,12,13);
+const int Tone =7;
+float temp;
+float tmpswitch;
+
+
+void setup()
+{
+  pinMode(3, OUTPUT);
+    Serial.begin(9600); // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+  lcd.begin(16,2);
+}
+
+void loop()
+{
+   digitalWrite(3, HIGH);
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+       Serial.println(buf[0]);
+        tone (Tone,659,250);
+        delay(250);
+        tone (Tone,440,250);
+        delay(250);
+    }
+    int Data =(buf[0] - 0);
+    if (Data == 1)
+    {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Tingkatan 1 ke");
+    lcd.setCursor(0,1);
+    lcd.print("tpk perhimpunan");
+    delay(100);
+    }
+    else if (Data == 2)
+    {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Tingkatan 2 ke");
+    lcd.setCursor(0,1);
+    lcd.print("tpk perhimpunan");
+    delay(100);
+    }
+    else if (Data == 3)
+    {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Tingkatan 3 ke");
+    lcd.setCursor(0,1);
+    lcd.print("tpk perhimpunan");
+    delay(100);
+    }
+    else if (Data == 10)
+    {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Outdoor Tempt.");
+    lcd.setCursor(0,1);
+    tmpswitch = (buf[1] - 0);
+    temp = (tmpswitch/5)+0.20;
+    lcd.print(temp);
+    lcd.setCursor(6,1);
+    lcd.print("Celcius");
+    delay(100);
+    }
+    else if (Data == 11)
+    {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Kebakaran");
+    lcd.setCursor(0,1);
+    lcd.print("Fire case");
+    tone (Tone,659,1000);
+    delay(1000);
+    tone (Tone,440,1000);
+    delay(1000);
+    }
+    else if (Data == 12)
+    {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Guru bertugas");
+    
+    if (driver.recv(buf, &buflen))
+      {
+       lcd.setCursor(0,1);
+       Serial.println((char*)buf);
+       lcd.print((char*)buf);
+      }
+    delay(100);
+    }
+    
+}
---
+++ TX2.ino
@@ -0,0 +1,74 @@
+#include <IRremote.h>
+//433MHz Libraries
+#include <RH_ASK.h>
+#include <SPI.h>
+RH_ASK driver;
+//IR Receiver libraries
+#include <IRremote.h>
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+//Declaration of pins
+char tmp[2] = {1};
+int analogVal;
+const int tempPin = A0;
+
+
+void setup()
+{
+    pinMode(10, OUTPUT);
+    Serial.begin(9600); 
+    irrecv.enableIRIn();
+    if (!driver.init()) Serial.println("init failed");
+}
+
+void loop()
+{
+   float voltage;
+   float temperature;
+   int tmp10;
+   analogVal = analogRead (tempPin);
+   temperature =(((5000.0/1024.0)*analogVal)-500)/10;
+   Serial.println(temperature);
+   tmp10 = temperature*5;
+   Serial.println(tmp10);
+   tmp[1] = char (tmp10);
+   if (irrecv.decode(&results))
+   {
+    Serial.println(results.value, HEX);
+    switch (results.value)
+    {
+      case 0x885B30D5:{tmp[0] = 1;
+      driver.send((uint8_t *)tmp, strlen(tmp));
+      driver.waitPacketSent();
+      break;}//Channel 1
+      case 0xE7E0CFB1:{tmp[0] = 2;
+      driver.send((uint8_t *)tmp, strlen(tmp));
+      driver.waitPacketSent();  
+      break;}//Channel 2
+      case 0xA34F5A01:{tmp[0] = 3;
+      driver.send((uint8_t *)tmp, strlen(tmp));
+      driver.waitPacketSent();  
+      break;}//Channel 3
+      case 0xC7E0BECF:{tmp[0] = 10;
+      driver.send((uint8_t *)tmp, strlen(tmp));
+      driver.waitPacketSent();  
+      break;}//Menu=Temp
+      case 0x28CBA803:{tmp[0] = 11;
+      driver.send((uint8_t *)tmp, strlen(tmp));
+      driver.waitPacketSent();  
+      break;}//Mute=Urgent
+      case 0xCC00A5B7:{tmp[0] = 12;
+      driver.send((uint8_t *)tmp, strlen(tmp));
+      driver.waitPacketSent();
+      char msg[] = "Lim";
+      Serial.println((char*)msg);
+      driver.send((uint8_t *)msg, strlen(msg));
+      driver.waitPacketSent();
+      break;}//AV=Normal text
+    }
+    irrecv.resume();
+   }  
+   digitalWrite(10, HIGH); //LED Pin
+   delay(500);
+}
